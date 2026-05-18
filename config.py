7179e2b7b813c7f25810d017da7f125669b8151c
Julio-Cesar-dos-Santos <juliocsantos05@gmail.com>
2026-05-18 16:17:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 HOST = "0.0.0.0"
-SSH_PORT = 2222       # porta alternativa para não conflitar com SSH real
+SSH_PORT = 2222       
 HTTP_PORT = 8888
 LOG_FILE = "logs/honeypot.log"
-ALERT_EMAIL = None    # coloque seu e-mail se quiser alertas
+ALERT_EMAIL = None   
